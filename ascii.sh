10077b94accf06fa65cfc2fd8d92117a7979670c
yogeshhhh <46068224+yogeshhhh@users.noreply.github.com>
2026-09-25 20:23:13 +0530

    Update ascii.sh

--- ascii.sh
+++ ascii.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install cowsay -y
+sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover , I am a dragon.... >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
